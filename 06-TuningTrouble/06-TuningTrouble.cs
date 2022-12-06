b73291546201084c32b136ff52e134e8c23a4259
JMulligan94 <j.mulligan94@hotmail.co.uk>
2022-12-06 12:42:24 +0000

    Day 6 Complete

---
+++ 06-TuningTrouble/06-TuningTrouble.cs
@@ -0,0 +1,50 @@
+﻿namespace _06_TuningTrouble
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var buffers = File.ReadAllLines(args[0]);
+
+			Console.WriteLine("=== Part One: ===");
+			foreach (var buffer in buffers)
+			{
+				FindMarker(buffer, 4);
+			}
+
+			Console.WriteLine("\n\n=== Part Two: ===");
+			foreach (var buffer in buffers)
+			{
+				FindMarker(buffer, 14);
+			}
+		}
+
+		static int FindMarker(string buffer, int distinctCount)
+		{
+			Console.WriteLine($"Searching {buffer} for {distinctCount} distinct characters");
+			var markerIndex = 0;
+			for (var bufferIndex = distinctCount - 1; bufferIndex < buffer.Length; ++bufferIndex)
+			{
+				var duplicate = false;
+				var pattern = buffer[bufferIndex - (distinctCount - 1)].ToString();
+				for (var i = distinctCount - 2; i >= 0; --i)
+				{
+					var nextChar = buffer[bufferIndex - i];
+					if (pattern.Contains(nextChar))
+					{
+						duplicate = true;
+						break;
+					}
+					pattern += nextChar;
+				}
+				if (!duplicate)
+				{
+					markerIndex = bufferIndex + 1;
+					break;
+				}
+			}
+			Console.WriteLine($"\tMarker: {markerIndex}");
+			return markerIndex;
+		}
+	}
+}
